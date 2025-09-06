10da20aba66fe8ab1f9a9394ddf365b0300ff068
TerezedegiKoyash <terezekoyash@gmail.com>
2025-09-06 13:51:59 +0300

    Add StaticElectricity class

--- electroPy/basicConcepts/basicConcepts.py
+++ electroPy/basicConcepts/basicConcepts.py
@@ -4,38 +4,76 @@ import math
 
 # 1.1 Static electricity 
 
-neutral_charge = 0 
-positive_charge = +1 
-negative_charge = -1 
+class StaticElectricity:
+    
+    def __init__(self):
+        self.charges = self.getCharges()
+        self.atomic_charge = self.getAtomicCharge()
+        self.electron_mobility = self.electronMobility()
+        self.atomic_identity = self.atomicIdentity()
+        self.isotope_example = self.isotopeExample()
+        
+    def getCharges(self):
+        """Define basic charge values for particles"""
+        neutral_charge = 0
+        positive_charge = +1
+        negative_charge = -1
+        neutrons_charge = neutral_charge
+        protonic_charge = positive_charge
+        electronic_charge = negative_charge
 
-neutrons_charge = 0 # Neutrons have a neutral (0) electric charge
-protonic_charge = +1 # Protons have a positive (+1) electric charge
-electronic_charge = -1 # Electrons have a negative (-1) electric charge
+        return {
+            'neutral_charge': neutral_charge,
+            'positive_charge': positive_charge,
+            'negative_charge': negative_charge,
+            'neutrons_charge': neutrons_charge,
+            'protonic_charge': protonic_charge,
+            'electronic_charge': electronic_charge
+        }
+    
+    def getAtomicCharge(self, protons, neutrons, electrons):
+        """Calculate total charge of an atom"""
+        proton_charge = protons * self.charges['positive_charge']
+        neutron_charge = neutrons * self.charges['neutral_charge']
+        electron_charge = electrons * self.charges['negative_charge']
+        
+        return proton_charge + neutron_charge + electron_charge
 
-electronic_charge == negative_charge # electrons_charge is the same as negative_charge
-protonic_charge == positive_charge # protons_charge is the same as positive_charge
-neutrons_charge == neutral_charge # neutrons_charge is the same as neutral_charge
+    def electronMobility(self):
+        """Properties of particle removal from atoms"""
+        return {
+            'electrons_easily_removed': True,
+            'protons_hardly_removed': False,
+            'neutrons_hardly_removed': False
+        }
 
-atomic_charge = positive_charge + negative_charge + neutral_charge # an atom is neutral if it has equal numbers of protons and electrons
-atomic_charge = electronic_charge + protonic_charge + neutrons_charge # an atom is neutral if it has equal numbers of protons and electrons
+    def atomicIdentity(self):
+        """Define particle types and element identity rule"""
+        
+        return {
+            'proton_count_determines_element': True,
+            'proton': 'proton',
+            'neutron': 'neutron',
+            'electron': 'electron'
+        }
+        
+    def isotopeExample(self):
+        """Create hydrogen-1 (protium) example"""
+        proton_count = 1
+        neutron_count = 0
+        electron_count = 1
 
-electrons_easily_removed = True
-protons_hardly_removed = False
-neutrons_hardly_removed = False # electrons can be dislodged from atoms much easier than protons or neutrons
-
-proton_count_determines_element = True # the number of protons in an atom's nucleus determines its identity as a unique element
-
-proton = 'proton'
-neutron = 'neutron'
-electron = 'electron'
-
-protius = {
-    proton: 1, 
-    neutron: 0, 
-    electron: 1
-    } # Hydrogen-1, the most common isotope of hydrogen, has 1 proton, 0 neutrons, and 1 electron
-protius_charge = protius[proton] + protius[electron] + protius[neutron] # the electric charge of protius is 0, because it has 1 proton (+1) and 1 electron (-1)
-
+        protius = {
+            'proton': proton_count,
+            'neutron': neutron_count,
+            'electron': electron_count
+        }
+        protius_charge = self.getAtomicCharge(proton_count, neutron_count, electron_count)
+        # Hydrogen-1, the most common isotope of hydrogen, has 1 proton, 0 neutrons, and 1 electron
+        # the electric charge of protius is 0, because it has 1 proton (+1) and 1 electron (-1)
+        
+        return protius, protius_charge
+        
 # 1.2 Conductors, insulators, and electron flow 
 
 conductors = True # in conductive materials, the outer electrons in each atom can easily come or go, and are called free electrons
